a7b0489cc9c88d7bc22c3ee1b7d90141ce1464db
jquinnie <jenn8866@gmail.com>
2018-10-11 15:05:39 -0600

    Create AuthController to test

---
+++ testsInMocha/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+const AuthController = () => {
+  function isAuthorized(roles, neededRole) {
+    return roles.indexOf(neededRole) >= 0;
+  }
+  return { isAuthorized };
+};
+
+module.exports = AuthController();
