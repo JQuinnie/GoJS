da508d9e22428a044fcb0be2a69db243ee884f34
JQuinn MacBook Air <jenn8866@gmail.com>
2018-10-14 15:41:53 -0600

    Change to function() not arrow for Mocha binding

--- testsInMocha/test/controllers/auth.controller.spec.js
+++ testsInMocha/test/controllers/auth.controller.spec.js
@@ -2,12 +2,12 @@ const assert = require('assert');
 
 const authController = require('../../controllers/auth.controller');
 
-describe('AuthController', () => {
-  describe('isAuthorized', () => {
-    it('should return false if not authorized', () => {
+describe('AuthController', function() {
+  describe('isAuthorized', function() {
+    it('should return false if not authorized', function() {
       assert.equal(false, authController.isAuthorized(['user'], 'admin'));
     });
-    it('should return true if authorized', () => {
+    it('should return true if authorized', function() {
       assert.equal(
         true,
         authController.isAuthorized(['user', 'admin'], 'admin')
@@ -15,7 +15,7 @@ describe('AuthController', () => {
     });
   });
 
-  describe('isAuthorizedAsync', () => {
+  describe('isAuthorizedAsync', function() {
     it('should return false if not authorized', function(done) {
       this.timeout(2500);
       authController.isAuthorizedAsync(['user'], 'admin', isAuth => {
