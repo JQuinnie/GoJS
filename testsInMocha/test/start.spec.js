91b3691b65d00915171c1f1c58f79ccdd4215f03
JQuinn MacBook Air <jenn8866@gmail.com>
2018-10-14 20:37:32 -0600

    Add examples for testing objects and null with should

--- testsInMocha/test/start.spec.js
+++ testsInMocha/test/start.spec.js
@@ -1,11 +1,25 @@
 // using assert as assertion framework
 const assert = require('assert');
+const should = require('chai').should();
 
 describe('Basic Mocha Test', () => {
   const thingOne = 1;
   const thingTwo = 2;
 
-  it('should throw errors if equal', () => {
+  it('should throw errors if equal', function() {
     assert.notEqual(thingOne, thingTwo);
+  });
+
+  it('should deal with objects', function() {
+    let obj1 = { name: 'Quinn', occupation: 'Developer' };
+    let obj2 = { name: 'Quinn', occupation: 'Developer' };
+    // use deep equal for object comparison
+    // failing test at obj1.should.equal(obj2)
+    obj1.should.deep.equal(obj2);
+  });
+
+  it('should allow testing nulls', function() {
+    let iAmNull = null;
+    should.not.exist(iAmNull);
   });
 });
