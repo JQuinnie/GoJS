ab54aa0aa402d9f597700687b0c6216b5d4b6c45
jquinnie <jenn8866@gmail.com>
2019-02-04 16:19:09 -0700

    Add example of async await

---
+++ asyncPromiseStuff/exampleAsyncAwait.js
@@ -0,0 +1,9 @@
+async function createFlow() {
+  console.log('Me first');
+  const data = await fetch('https://www.anapioficeandfire.com/api/houses/378');
+  console.log(data);
+}
+
+createFlow();
+
+console.log('Me second');
