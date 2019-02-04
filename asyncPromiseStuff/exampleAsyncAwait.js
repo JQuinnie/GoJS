77f605e4e411a342f07d4087889f0f75be88e34e
jquinnie <jenn8866@gmail.com>
2019-02-04 16:33:34 -0700

    Add packages to run fetch

--- asyncPromiseStuff/exampleAsyncAwait.js
+++ asyncPromiseStuff/exampleAsyncAwait.js
@@ -1,3 +1,6 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 async function createFlow() {
   console.log('Me first');
   const data = await fetch('https://www.anapioficeandfire.com/api/houses/378');
