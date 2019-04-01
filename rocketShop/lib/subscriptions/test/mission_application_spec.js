de122264c1d3b84b1a718cb75924a97e62761c29
JQ <jenn8866@gmail.com>
2019-03-31 21:02:58 -0600

    Add node testing project with starter files

---
+++ rocketShop/lib/subscriptions/test/mission_application_spec.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const MembershipApplication = require('../../membership_application');
+
+describe('Memberhsip application requirements', function() {
+  let validApp;
+
+  before(function() {
+    //arrange the data here
+    validApp = new MembershipApplication({
+      first: 'Test',
+      last: 'User',
+      email: 'test@test.com',
+      age: 30,
+      height: 66,
+      weight: 180
+    });
+  });
+
+  describe('Application valid if...', function() {
+    it('all validators successful', function() {
+      assert(validApp.isValid(), 'Not valid');
+    });
+    it('email is 4 or more chars and contains an @', function() {
+      assert(validApp.emailIsValid());
+    });
+    it('height is between 60 and 75 inches', function() {
+      assert(validApp.heightIsValid());
+    });
+    it('age is betwen 15 and 100', function() {
+      assert(validApp.ageIsValid());
+    });
+    it('weight is between 100 and 300', function() {
+      assert(validApp.weightIsValid());
+    });
+    it('first and last name are provided', function() {
+      assert(validApp.nameIsValid());
+    });
+  });
+});
