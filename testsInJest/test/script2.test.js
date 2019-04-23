d76c3bcc639869c66430a6cd5479c1c8090d7269
jquinnie <jenn8866@gmail.com>
2019-04-23 17:05:28 -0600

    Add additional async tests

--- testsInJest/test/script2.test.js
+++ testsInJest/test/script2.test.js
@@ -1,2 +1,20 @@
 const fetch = require('node-fetch');
 const swapi = require('../script2');
+
+// passing done will tell Jest to not run test until it sees done, done usually comes after expect
+it('calls swapi to get people', (done) => {
+  expect.assertions(1);
+  swapi.getPeople(fetch).then((data) => {
+    expect(data.count).toEqual(87);
+    done();
+  });
+});
+
+// another way for async test is to return the promise
+it('calls swapi to get people with a promise', () => {
+  expect.assertions(2);
+  return swapi.getPeoplePromise(fetch).then((data) => {
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
