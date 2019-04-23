7aaa4ca74d8b6fb1a8b8ab43f6375357286d8527
jquinnie <jenn8866@gmail.com>
2019-04-23 12:04:47 -0600

    Updte app to call db

--- testsInJest/script.js
+++ testsInJest/script.js
@@ -1,10 +1,12 @@
 const moogleDb = require('./moogleDb');
 
-const moogleSearch = (searchInput) => {
+const moogleSearch = (searchInput, db) => {
   // filter each website that includes the search input
-  const matches = moogleDb.filter(website => website.includes(searchInput));
+  const matches = db.filter(website => website.includes(searchInput));
   // if there are more than 3 matches then just return the top 3
   return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-console.log(moogleSearch('recipe'));
+// console.log(moogleSearch('recipe', moogleDb));
+
+module.exports = moogleSearch;
