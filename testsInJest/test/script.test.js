34781452ae4e1803937f0ccd8bd578e8acc69ded
jquinnie <jenn8866@gmail.com>
2019-04-23 12:12:40 -0600

    Update with more tests

--- testsInJest/test/script.test.js
+++ testsInJest/test/script.test.js
@@ -9,11 +9,24 @@ const dbMock = [
   'nba.com',
 ];
 
-it('this is an example test', () => {
-  expect('hello world').toBe('hello world');
+describe('first Jest test', () => {
+  it('passes an example test', () => {
+    expect('hello world').toBe('hello world');
+  });
 });
 
-it('is searching moogle', () => {
-  expect(moogleSearch('test', dbMock)).toEqual([]);
-  expect(moogleSearch('mock', dbMock)).toEqual(['mock.com', 'mock2.com', 'mock3.com']);
+describe('moogleSearch', () => {
+  it('is searching moogle', () => {
+    expect(moogleSearch('test', dbMock)).toEqual([]);
+    expect(moogleSearch('mock', dbMock)).toEqual(['mock.com', 'mock2.com', 'mock3.com']);
+  });
+
+  it('works with undefined and null input', () => {
+    expect(moogleSearch(undefined, dbMock)).toEqual([]);
+    expect(moogleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it('does not return more than 3 matches', () => {
+    expect(moogleSearch('.com', dbMock).length).toEqual(3);
+  });
 });
