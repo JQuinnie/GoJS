aaf5bee53218e3532d959a4ff1e33bd91124ff24
jquinnie <jenn8866@gmail.com>
2019-04-24 09:43:30 -0600

    Add note

--- testsInJest/test/script2.test.js
+++ testsInJest/test/script2.test.js
@@ -3,7 +3,7 @@ const swapi = require('../script2');
 
 // passing done will tell Jest to not run test until it sees done, done usually comes after expect
 it('calls swapi to get people', (done) => {
-  expect.assertions(1);
+  expect.assertions(1); // This is often useful when testing asynchronous code, in order to make sure that assertions in a callback actually got called.
   swapi.getPeople(fetch).then((data) => {
     expect(data.count).toEqual(87);
     done();
