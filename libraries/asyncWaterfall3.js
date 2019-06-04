92799bfa52164c6304c2c243944a53f1a1210b00
jquinnie <jenn8866@gmail.com>
2019-06-04 16:19:52 -0600

    Add notes

--- libraries/asyncWaterfall3.js
+++ libraries/asyncWaterfall3.js
@@ -6,25 +6,20 @@ const octokit = new Octokit({
   baseUrl: 'https://api.github.com',
 });
 
+const getUser = (username, callback) => {
+  octokit.users.getByUsername({ username })
+    .then(
+      (result) => {
+        callback(undefined, result.data.avatar_url);
+      },
+    )
+    .catch(callback);
+};
+
 // do multiple things in order
 async.waterfall([
-  // first function will get the url from github
-  // because the call to github is a callback, we have to use then/catch
-  (callback) => {
-    octokit.users.getByUsername({ username: 'airbnb' })
-      .then(
-        (result) => {
-          callback(null, result.data.avatar_url);
-        },
-      )
-      .catch(callback);
-  },
-  // example of the above using async / await
-  // async function getUserAvatar(callback) {
-  //   const getUserName = await octokit.users.getByUsername({ username: 'airbnb' });
-  //   const avatarUrl = getUserName.data.avatar_url;
-  //   return avatarUrl;
-  // },
+  // return function that accepts a function and returns a function that accept a callback
+  async.apply(getUser, 'airbnb'),
   // second function will append the url to the img tag
   function wrapAvatarInHtml(avatarUrl, callback) {
     const avatarWithHtml = `<img src="${avatarUrl}"/>`;
