78d36515188089338f27d19df20e23837ca58855
jquinnie <jenn8866@gmail.com>
2019-12-03 20:35:02 -0700

    Update formatting

--- advanceJs/implementFilterPrototype.js
+++ advanceJs/implementFilterPrototype.js
@@ -1,21 +1,18 @@
 // Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You may use a for loop or the Array.prototype.forEach() method.
 
 // the global Array
-var s = [23, 65, 98, 5];
+const s = [23, 65, 98, 5];
 
-Array.prototype.myFilter = function(callback){
-  var newArray = [];
+Array.prototype.myFilter = function (callback) {
+  const newArray = [];
   // Add your code below this line
-  this.forEach(item => {
-    if(callback(item)===true){
-    newArray.push(item)
+  this.forEach((item) => {
+    if (callback(item) === true) {
+      newArray.push(item);
     }
-  })
+  });
   // Add your code above this line
   return newArray;
-
 };
 
-var new_s = s.myFilter(function(item){
-  return item % 2 === 1;
-});
+const new_s = s.myFilter((item) => item % 2 === 1);
